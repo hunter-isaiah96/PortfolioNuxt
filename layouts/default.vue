79d5d8002394dbeb12e2ded7a78e95853f6f2808
Isaiah Hunter <isaiah@Dres-Macbook.fios-router.home>
2020-01-07 03:02:52 -0500

    Design Overhaul

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,61 +1,116 @@
 <template>
   <div>
-    <v-app style="background-color: #212735;">
-      <v-content>
-        <Header></Header>
-        <v-container style="max-width: 1500px">
-          <!-- <transition name="slide-x-fade" mode="out-in"> -->
-          <nuxt />
-          <!-- </transition> -->
-        </v-container>
-        <v-navigation-drawer v-model="drawer" absolute temporary>
-          <v-list-item-group v-model="group">
-            <v-list-item>
-              <v-list-item-title>Foo</v-list-item-title>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-title>Bar</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-navigation-drawer>
-      </v-content>
+    <v-app style="background-color: #000;">
+      <v-container class="pa-0" fluid>
+        <v-row class="navigation headline" color="transparent">
+          <v-col cols="auto" class="mb-4">Intro</v-col>
+          <v-col cols="auto" class="mb-4">Portfolio</v-col>
+          <v-col cols="auto">Blog</v-col>
+        </v-row>
+        <v-row class="full-height" no-gutters>
+          <v-col class="pr-6" cols="6">
+            <v-row class="fill-height" align-content="center" justify="end">
+              <v-col class="intro-content pl-5" cols="auto" style="max-width: 400px;">
+                <h1 class="text-right mb-6 display-1 font-weight-bold">Isaiah Hunter</h1>
+                <!-- <v-img class="mb-6 ml-auto" :src="require('../assets/logo.svg')" width="50%"></v-img> -->
+                <p
+                  class="mb-6"
+                >Welcome! I’m Isaiah and clearly, I need professional photos. I’m a passionate self taught developer & graphic designer, check out some of my work and feel free to contact me.</p>
+                <v-row justify="end">
+                  <v-col cols="auto" v-for="(link, i) in social" :key="i">
+                    <v-img height="20" width="20" :src="require(`../assets/${link.icon}.svg`)"></v-img>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="6">
+            <v-img class="intro-image" :src="me" height="100%"></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+      <nuxt />
     </v-app>
   </div>
 </template>
-<script>
-import Header from '@/components/Header'
 
+<script>
+import me from '@/assets/me.jpg'
 export default {
-  components: {
-    Header
-  },
   data() {
     return {
-      items: [
-        { title: 'Home', to: '/' },
-        { title: 'Dev', to: '/dev' },
-        { title: 'Design', to: '/design' }
+      me,
+      social: [
+        {
+          icon: 'github-icon'
+        },
+        {
+          icon: 'instagram-icon'
+        },
+        {
+          icon: 'uplabs-icon'
+        },
+        {
+          icon: 'email-icon'
+        }
       ]
-    }
-  },
-  computed: {
-    drawer: {
-      get() {
-        return this.$store.state.menu.drawer
-      },
-      set(value) {
-        this.$store.dispatch('menu/toggleDrawer', value)
-      }
-    },
-    group: {
-      get() {
-        return this.items.findIndex(item => item.to === this.$route.path)
-      },
-      set(value) {
-        this.$store.dispatch('menu/toggleDrawer', false)
-        return value
-      }
     }
   }
 }
 </script>
+<style lang="scss">
+.intro-image .v-responsive__sizer {
+  padding-bottom: 0 !important;
+}
+</style>
+<style lang="scss" scoped>
+.intro-content {
+  position: relative;
+  overflow: hidden;
+  &:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    z-index: 1;
+    animation: a-ltr-before 1.5s cubic-bezier(0.77, 0, 0.18, 1) forwards;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    animation: a-ltr-after 1.5s cubic-bezier(0.77, 0, 0.18, 1) forwards;
+  }
+}
+@keyframes a-ltr-after {
+  0% {
+    transform: translateX(110%);
+  }
+  100% {
+    transform: translateX(-99%);
+  }
+}
+
+@keyframes a-ltr-before {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-110%);
+  }
+}
+.navigation {
+  position: fixed;
+  left: 80px;
+  top: 40px;
+  transform: rotate(90deg);
+  transform-origin: top left;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,63 @@
 <template>
-  <div>
-    <v-container fluid>
-      <v-row dense>
-        <v-col
-          class="py-0"
-          style="position: relative;"
-          v-for="(post, index) in posts"
-          :key="index"
-          cols="4"
-        >
-          <dev-design v-if="post.sys.contentType.sys.id === 'project'" :post="post"></dev-design>
-          <!-- <podcast v-if="item.type === 'podcast'" :post="item"></podcast> -->
-        </v-col>
-      </v-row>
-      <!-- <v-pagination v-if="total_pages > 1" v-model="page" :length="total_pages" circle></v-pagination> -->
-    </v-container>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import { createClient } from '~/plugins/contentful.js'
-const client = createClient()
-import image from '@/assets/me.jpg'
+export default {}
+</script>
 
-import DevDesign from '@/components/DevDesign'
-import Podcast from '@/components/Podcast'
-
-export default {
-  components: {
-    DevDesign,
-    Podcast
-  },
-  data: () => ({
-    title: "Isaiah Hunter's Portfolio",
-    description: 'This is the portfolio of Isaiah Hunter',
-    type: 'projects'
-  }),
-  head() {
-    return {
-      title: "Isaiah Hunter's Portfolio",
-      meta: [
-        { hid: 'description', name: 'description', content: this.description },
-        {
-          hid: 'og:description',
-          name: 'og:description',
-          content: this.description
-        },
-        { hid: 'og:title', name: 'og:title', content: this.title },
-        {
-          hid: 'og:image',
-          name: 'og:image',
-          content: process.env.baseUrl + image.substring(1)
-        },
-        { hid: 'twitter:title', name: 'twitter:title', content: this.title },
-        {
-          hid: 'twitter:description',
-          name: 'twitter:description',
-          content: this.description
-        },
-        {
-          hid: 'twitter:image',
-          name: 'twitter:image',
-          content: process.env.baseUrl + image.substring(1)
-        }
-      ]
-    }
-  },
-  async asyncData({ error, req }) {
-    try {
-      const data = await client.getEntries({ limit: 15 })
-      return { posts: data.items }
-    } catch (e) {
-      console.log(e)
-      error({ statusCode: 404, message: 'There was a problem querying' })
-    }
-  }
-}
+<script>
+// import { createClient } from '~/plugins/contentful.js'
+// const client = createClient()
+// import image from '@/assets/me.jpg'
+// export default {
+//   components: {
+//     DevDesign,
+//     Podcast
+//   },
+//   data: () => ({
+//     title: "Isaiah Hunter's Portfolio",
+//     description: 'This is the portfolio of Isaiah Hunter',
+//     type: 'projects'
+//   }),
+//   head() {
+//     return {
+//       title: "Isaiah Hunter's Portfolio",
+//       meta: [
+//         { hid: 'description', name: 'description', content: this.description },
+//         {
+//           hid: 'og:description',
+//           name: 'og:description',
+//           content: this.description
+//         },
+//         { hid: 'og:title', name: 'og:title', content: this.title },
+//         {
+//           hid: 'og:image',
+//           name: 'og:image',
+//           content: process.env.baseUrl + image.substring(1)
+//         },
+//         { hid: 'twitter:title', name: 'twitter:title', content: this.title },
+//         {
+//           hid: 'twitter:description',
+//           name: 'twitter:description',
+//           content: this.description
+//         },
+//         {
+//           hid: 'twitter:image',
+//           name: 'twitter:image',
+//           content: process.env.baseUrl + image.substring(1)
+//         }
+//       ]
+//     }
+//   },
+//   async asyncData({ error, req }) {
+//     try {
+//       const data = await client.getEntries({ limit: 15 })
+//       return { posts: data.items }
+//     } catch (e) {
+//       console.log(e)
+//       error({ statusCode: 404, message: 'There was a problem querying' })
+//     }
+//   }
+// }
 </script>
