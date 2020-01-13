4a50c6dedc64cedbb59eaaa7c0c00c00011bc24c
Isaiah Hunter <isaiah@Dres-Macbook.fios-router.home>
2020-01-13 18:40:51 -0500

    changed interface

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,145 +1,62 @@
 <template>
   <div>
-    <div class="cursor"></div>
-    <v-app style="background-color: #111;">
-      <v-container class="pa-0" fluid>
-        <!-- <v-row class="navigation headline" color="transparent">
-          <v-col cols="auto" class="mb-4">Intro</v-col>
-          <v-col cols="auto" class="mb-4">Portfolio</v-col>
-          <v-col cols="auto">Blog</v-col>
-        </v-row>-->
-        <v-row class="full-height" no-gutters>
-          <v-col class="pr-6" cols="6">
-            <v-row class="fill-height" align-content="center" justify="end">
-              <v-col class="intro-content pl-5" cols="auto" style="max-width: 400px;">
-                <h1 class="text-right mb-6 display-1 font-weight-bold">Isaiah Hunter</h1>
-                <!-- <v-img class="mb-6 ml-auto" :src="require('../assets/logo.svg')" width="50%"></v-img> -->
-                <p
-                  class="mb-6 text-right"
-                >Welcome! I’m Isaiah and clearly, I need professional photos. I’m a passionate self taught developer & graphic designer, check out some of my work and feel free to contact me.</p>
-                <v-row justify="end">
-                  <v-col cols="auto" v-for="(link, i) in social" :key="i">
-                    <nuxt-link to="/">
-                      <v-img height="20" width="20" :src="require(`../assets/${link.icon}.svg`)"></v-img>
-                    </nuxt-link>
-                  </v-col>
-                </v-row>
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col md="6">
-            <v-img class="intro-image" :src="me" height="100vh" transition="slide-y-transition"></v-img>
-          </v-col>
-        </v-row>
-      </v-container>
+    <v-app class="mx-auto" style="background-color: white;">
       <nuxt />
+      <transition name="fade">
+        <div class="nav-overlay" v-show="drawer">
+          <v-container class="full-height" style="max-width: 1024px;">
+            <v-toolbar
+              color="transparent"
+              class="px-0"
+              elevation="0"
+              max-height="128"
+            >
+              <v-spacer></v-spacer>
+              <button
+                :class="{ 'is-active': drawer }"
+                class="hamburger hamburger--squeeze"
+                type="button"
+                @click="toggleMenu()"
+              >
+                <span class="hamburger-box">
+                  <span class="hamburger-inner"></span>
+                </span>
+              </button>
+            </v-toolbar>
+          </v-container>
+        </div>
+      </transition>
     </v-app>
   </div>
 </template>
-
+<style lang="scss" scoped>
+.nav-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  background: rgba(0, 0, 0, 0.87);
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
 <script>
-import me from '@/assets/me.jpg'
+import { mapGetters } from 'vuex'
 export default {
-  data() {
-    return {
-      me,
-      social: [
-        {
-          icon: 'github-icon'
-        },
-        {
-          icon: 'instagram-icon'
-        },
-        {
-          icon: 'uplabs-icon'
-        },
-        {
-          icon: 'email-icon'
-        }
-      ]
-    }
+  computed: {
+    ...mapGetters({
+      drawer: 'menu/drawer'
+    })
   },
-  mounted() {
-    let moveWithCursor = true
-    let cursorA = document.getElementsByClassName('cursor')[0]
-    let links = document.getElementsByTagName('a')
-    // for (let link of links) {
-    //   link.addEventListener('mouseover', e => {
-    //     cursorA.classList.add('expand')
-    //   })
-    //   link.addEventListener('mouseout', e => {
-    //     defaultCursor()
-    //   })
-    // }
-
-    document.addEventListener('mousemove', e => {
-      if (moveWithCursor) {
-        cursorA.style.left = `${e.pageX}px`
-        cursorA.style.top = `${e.pageY}px`
-      }
-    })
-    document.addEventListener('mousedown', e => {
-      cursorA.classList.add('expand')
-    })
-    document.addEventListener('mouseup', e => {
-      cursorA.classList.remove('expand')
-    })
+  methods: {
+    toggleMenu() {
+      this.$store.commit('menu/TOGGLE_DRAWER', !this.drawer)
+    }
   }
 }
 </script>
-<style lang="scss">
-.intro-image .v-responsive__sizer {
-  padding-bottom: 0 !important;
-}
-</style>
-<style lang="scss" scoped>
-.intro-content {
-  position: relative;
-  overflow: hidden;
-  &:before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 110%;
-    height: 100%;
-    background-color: #111;
-    z-index: 1;
-    animation: a-ltr-before 1.5s cubic-bezier(0.77, 0, 0.18, 1) forwards;
-  }
-  &:after {
-    content: '';
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background-color: white;
-    animation: a-ltr-after 1.5s cubic-bezier(0.77, 0, 0.18, 1) forwards;
-  }
-}
-@keyframes a-ltr-after {
-  0% {
-    transform: translateX(110%);
-  }
-  100% {
-    transform: translateX(-99%);
-  }
-}
-
-@keyframes a-ltr-before {
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(-110%);
-  }
-}
-.navigation {
-  position: fixed;
-  left: 80px;
-  top: 40px;
-  transform: rotate(90deg);
-  transform-origin: top left;
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,131 @@
 <template>
-  <div></div>
+  <div>
+    <v-container class="d-flex flex-column full-height text-center" fluid>
+      <v-toolbar class="px-0" elevation="0" max-height="128">
+        <v-toolbar-title>Isaiah Hunter</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <button
+          :class="{ 'is-active': drawer }"
+          class="hamburger hamburger--squeeze"
+          type="button"
+          @click="toggleMenu()"
+        >
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+      </v-toolbar>
+      <v-row
+        style="padding-bottom: 128px;"
+        class="flex-grow-1"
+        align="center"
+        no-gutters
+      >
+        <v-col md="8" xs="12" class="mx-auto">
+          <v-avatar size="180" class="mb-12"
+            ><v-img :src="require('../assets/me.jpg')"></v-img
+          ></v-avatar>
+          <h1 class="black--text display-3 font-weight-bold mb-2">
+            Isaiah Hunter
+          </h1>
+          <h1 class="mt-12 font-weight-light grey--text text--darken-1">
+            I've been working in the tech industry for {{ years }} years. I am
+            passionate about code, design and graphics. Here's some of my work.
+          </h1>
+        </v-col>
+      </v-row>
+      <v-icon class="chev">mdi-chevron-down</v-icon>
+    </v-container>
+    <v-container>
+      <v-row>
+        <v-col
+          class="pa-sm-12 pa-xs-0"
+          cols="12"
+          sm="6"
+          v-for="i in 2"
+          :key="i"
+        >
+          <v-img
+            :src="
+              require('../assets/krisztian-tabori-IyaNci0CyRk-unsplash.jpg')
+            "
+            max-height="260px"
+            min-height="260px"
+          ></v-img>
+          <h2 class="font-weight-medium mt-5">
+            Design doesn't have to be difficult
+          </h2>
+          <h3 class="font-weight-regular grey--text text--darken-3 my-5">
+            Stop approaching design as something that has to be difficult to be
+            beautiful
+          </h3>
+          <h3 class="font-weight-regular font-weight-regular">
+            <a href="#" class="underlined grey--text text--darken-3"
+              >Read the article</a
+            >
+          </h3>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+<style lang="scss" scoped>
+.mouse {
+  animation: mover 1s infinite;
+}
+@keyframes mover {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(20px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+.menu-button {
+  width: 30px;
+  height: 20px;
+  position: relative;
+  cursor: pointer;
+}
+.chev {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
 
 <script>
-export default {}
-</script>
-
-<script>
+import { mapGetters } from 'vuex'
+export default {
+  data() {
+    return {
+      years: new Date().getFullYear() - 2014,
+      menu_open: false
+    }
+  },
+  computed: {
+    ...mapGetters({
+      drawer: 'menu/drawer'
+    }),
+    fontSize() {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+          return '3em'
+        default:
+          return '5em'
+      }
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.$store.commit('menu/TOGGLE_DRAWER', !this.drawer)
+    }
+  }
+}
 // import { createClient } from '~/plugins/contentful.js'
 // const client = createClient()
 // import image from '@/assets/me.jpg'
